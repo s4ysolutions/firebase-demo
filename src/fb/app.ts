920b601a9a78b231dbf509d959783b0913bf9f92
Sergey Dolin <sergey.dolin@gmail.com>
2022-11-11 21:02:20 +0100

    Add AppCheck

--- src/fb/app.ts
+++ src/fb/app.ts
@@ -1,5 +1,16 @@
-import { initializeApp } from 'firebase/app';
+import {initializeApp} from 'firebase/app';
 import firebaseConfig from './config';
 
+import {ReCaptchaV3Provider, initializeAppCheck} from 'firebase/app-check';
+
+import appCheckJSON from './appCheck.json';
+
 const app = initializeApp(firebaseConfig);
+(<any>self).FIREBASE_APPCHECK_DEBUG_TOKEN = appCheckJSON.debug;
+export const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider(appCheckJSON.key),
+
+  isTokenAutoRefreshEnabled: true,
+});
+
 export default app;
